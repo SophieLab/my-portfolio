e4c5f4e182011a2d1b5869fd24ca5f07da011474
Sophie Labyt <labytsophie@gmail.com>
2024-10-08 21:01:29 +0200

    Initialize project using Create React App

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: 'localhost', // Empêche l'affichage de l'IP réseau
+    port: 3000, // Le port que vous souhaitez utiliser
+    strictPort: true, // Assure que le port soit utilisé, pas d'autres ports
+  },
+});
