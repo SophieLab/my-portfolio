469b27570dc254c0d9dfe1850f2d6145df9a13a6
Sophie <labytsophie@gmail.com>
2024-11-08 13:44:23 +0100

    wip

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
-import next from 'next';
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-const isProd = process.env.NODE_ENV === 'production';
-
-export default {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  assetPrefix: isProd ? '/my-portfolio/' : '',
-  basePath: isProd ? '/my-portfolio' : '',
-  output: 'export',
-};
+export default nextConfig;
