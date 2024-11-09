85b43b56e38a32133952c209f1ef1cddab238fa6
Sophie <labytsophie@gmail.com>
2024-11-09 22:06:01 +0100

    test

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -19,12 +19,12 @@ const Stats = () => {
       <div className="container mx-auto">
         <div className="w-full inline-flex flex-nowrap overflow-hidden">
           <ul className="flex items-center animate-infinite-scroll whitespace-nowrap">
-            {stats.concat(stats).map((item, index) => ( // Duplique la liste pour un défilement infini
+            {stats.concat(stats).map((item, index) => (
               <li
-                key={index}
-                className={`flex-none bg-transparent border border-white border-opacity-40 text-white opacity-40 py-2 px-4 rounded-full shadow-lg mx-2`}
+                key={`${item.text}-${index}`} // Clé unique combinant le texte et l'index
+                className="flex-none bg-transparent border border-white border-opacity-40 text-white opacity-40 py-2 px-4 rounded-full shadow-lg mx-2"
               >
-                <p className="text-lg xl:text-xl font-medium opacity-40"> {/* Opacité de 40 ici aussi */}
+                <p className="text-lg xl:text-xl font-medium opacity-40">
                   {item.text}
                 </p>
               </li>
@@ -35,7 +35,7 @@ const Stats = () => {
       <style jsx>{`
         .animate-infinite-scroll {
           display: flex;
-          animation: scroll 20s linear infinite; /* Durée de l'animation */
+          animation: scroll 20s linear infinite; /* Ajustez la vitesse du défilement */
         }
 
         @keyframes scroll {
@@ -43,8 +43,13 @@ const Stats = () => {
             transform: translateX(0);
           }
           100% {
-            transform: translateX(-50%); /* Défilement sur 50% de la largeur */
+            transform: translateX(-50%); /* Assurez un défilement fluide sur la moitié de la largeur */
           }
+        }
+
+        /* Optionnel : Ajouter un effet au survol pour augmenter l'opacité */
+        ul li:hover p {
+          opacity: 1;
         }
       `}</style>
     </section>
